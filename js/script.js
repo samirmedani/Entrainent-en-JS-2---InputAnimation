5667ed37317d3f6b8b2b042093ea3b68b07effc5
Mayuu_ <40952685+samirmedani@users.noreply.github.com>
2022-04-13 18:55:01 +0200

    Projet terminé et fonctionnel

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
+
+// Je sélectionne mon input grâce à la propriété querySelector 
 const validationInput = document.querySelector('input');  
 
+// je crée une condition indiquant que si la longueur des valeurs
+// présente dans mon input est inférieure à 3 elle reverra sur rouge en bordure
+// sinon elle renverra du vert, utilise pour des formulaires
 validationInput.addEventListener('input', (e) => {
    
     console.log(e.target.value)
